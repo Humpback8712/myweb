abc7ebb344afb38492d109ca0f8049b1d99cf5d1
yuhao <1278412363@qq.com>
2020-04-09 20:15:18 +0800

    增加了博客首页

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import index
+from .views import index, blog_index
 
+app_name = 'app'
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('blog_index', blog_index, name='blog_index')
 ]
